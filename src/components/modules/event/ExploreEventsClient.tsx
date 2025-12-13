f6670fe105c89850167e516b585d675e38324371
md.munna <mk8761174@gmail.com>
2025-12-13 18:01:31 +0600

    change layout

--- src/components/modules/event/ExploreEventsClient.tsx
+++ src/components/modules/event/ExploreEventsClient.tsx
@@ -152,7 +152,7 @@ const ExploreEventsClient = ({ initialEvents, initialMeta }: ExploreEventsClient
                         </CardContent>
                     </Card>
                 ) : (
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
                     {events.map((event) => (
                         <Card 
                             key={event.id} 
